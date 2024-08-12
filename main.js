a1237a6884419c4f33f3be04ad50aca7fab70f0f
Idris <idriszx638@gmail.com>
2024-08-12 11:21:39 +0700

    mengimpor body dan Jib

---
+++ main.js
@@ -0,0 +1,11 @@
+import { } from 'https://www.gstatic.com/firebasejs/10.4.0/firebase-SERVICE.js'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCRRFaBrm14cIoJ1nW5knt4Afd10H402Lo",
+    authDomain: "insan-cemerlang-bbed3.firebaseapp.com",
+    projectId: "insan-cemerlang-bbed3",
+    storageBucket: "insan-cemerlang-bbed3.appspot.com",
+    messagingSenderId: "1014883164148",
+    appId: "1:1014883164148:web:bba81cbf989594dfeee3a1",
+    measurementId: "G-9S4YKP6VBB"
+  };
